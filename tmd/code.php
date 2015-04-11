a6e5782ac57ba8b2708fe6d9db9ed5bfa0c19962
cos800 <thisky@qq.com>
2015-04-11 14:16:21 +0800

    xxx

---
+++ tmd/code.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15-4-11
+ * Time: 下午2:12
+ */
+
+namespace tmd;
+
+
+class code {
+    function base64()
+    {
+        
+    }
+}
