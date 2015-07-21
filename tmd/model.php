ec7ca5bfd51f05fc7279cfcaac1047ac179fdb66
cos800 <thisky@qq.com>
2015-07-21 16:23:38 +0800

    add model::optionHtml()

--- tmd/model.php
+++ tmd/model.php
@@ -2,5 +2,13 @@
 namespace tmd;
 
 class model {
-
+    static function optionHtml($prop)
+    {
+        $a = static::$$prop;
+        $html = '';
+        foreach ($a as $k=>$v) {
+            $html .= "<option value=\"$k\">$v</option>";
+        }
+        return $html;
+    }
 }
